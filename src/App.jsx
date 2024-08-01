562ac124aa41ebe442b89f9100d49a003520cb3d
D.Senal Danushka Galagedara <senalgalagedara3@gmail.com>
2024-08-01 21:18:01 +0530

    audit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React  from 'react';
+import MuiTypography from './components/MuiTypography';
+function App() {
+  return (
+    <div className="App">
+      <MuiTypography />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,17 @@
+import React  from 'react';
+import { Typography } from "@mui/material"
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant ='h1'>h1 Heading</Typography>
+      <Typography variant ='h2'>h2 Heading</Typography>
+      <Typography variant ='h3'>h3 Heading</Typography>
+      <Typography variant ='h4'>h4 Heading</Typography>
+      <Typography variant ='h5'>h5 Heading</Typography>
+
+    </div>
+  )
+}
+
+export default MuiTypography
